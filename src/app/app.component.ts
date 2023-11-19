33d206c300bd1c238519485e9e4c068557e1660d
Umar Turdiev <ukcturdiev@gmail.com>
2023-11-18 20:58:23 -0800

    Create root UI layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import { AssetBrowserPanelComponent } from './components/panel/asset-browser-panel.component';
+import { SceneExplorerPanelComponent } from './components/panel/scene-explorer-panel.component';
+import { PropertiesPanelComponent } from './components/panel/properties-panel.component';
+import { PhysicsComponent } from './components/physics/physics.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { BasePanelComponent } from './components/panel/base-panel.component';
+import { AssetBrowserPanelComponent } from './components/panel/asset-browser-panel.component';
+import { SceneExplorerPanelComponent } from './components/panel/scene-explorer-panel.component';
+import { PropertiesPanelComponent } from './components/panel/properties-panel.component';
+import { PhysicsComponent } from './components/physics/physics.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
+import { TextboxUpDownComponent } from './components/textbox-up-down/textbox-up-down.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     BasePanelComponent,
+    AssetBrowserPanelComponent,
+    SceneExplorerPanelComponent,
+    PropertiesPanelComponent,
+    PhysicsComponent,
+    DropdownComponent,
+    TextboxUpDownComponent,
+  ],
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
