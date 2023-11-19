20fbee3374c963f7ac0ac9520f6ffa56c32b691d
Umar Turdiev <ukcturdiev@gmail.com>
2023-11-18 20:55:50 -0800

    Add custom dropdown

---
+++ src/app/components/dropdown/dropdown.component.ts
@@ -0,0 +1,8 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'dropdown',
+  templateUrl: './dropdown.component.html',
+  styleUrls: ['./dropdown.component.css', '../../../styles.css'],
+})
+export class DropdownComponent {}
