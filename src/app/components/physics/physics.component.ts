7c87ace7d2bdc0c7f214d54b159d505269987bc7
Umar Turdiev <ukcturdiev@gmail.com>
2023-11-20 01:47:39 +0000

    Add more objects to demo scene

--- src/app/components/physics/physics.component.ts
+++ src/app/components/physics/physics.component.ts
@@ -44,31 +44,66 @@ export class PhysicsComponent implements AfterViewInit {
   }
 
   private setupScene() {
-    const boxA = Matter.Bodies.rectangle(200, 200, 200, 30, {
+    const floor = window.innerHeight - 60;
+
+    const boxA = Matter.Bodies.rectangle(200, floor, 30, 200, {
+      mass: 5,
+      friction: 1.0,
+    });
+
+    const boxB = Matter.Bodies.rectangle(250, 50, 250, 40, {
+      mass: 3,
+      friction: 1.0,
+    });
+
+    const boxC = Matter.Bodies.rectangle(350, floor, 30, 200, {
+      mass: 5,
+      friction: 1.0,
+    });
+
+    const boxD = Matter.Bodies.rectangle(430, 100, 200, 30, {
       mass: 10,
       friction: 1.0,
     });
 
-    const boxB = Matter.Bodies.rectangle(250, 50, 80, 80, {
-      mass: 10,
-      friction: 0.1,
-    });
-
-    const boxC = Matter.Bodies.rectangle(300, 200, 200, 30, {
-      mass: 10,
-      friction: 1.0,
-    });
-
-    const boxD = Matter.Bodies.rectangle(400, 100, 200, 30, {
-      mass: 10,
-      friction: 1.0,
-    });
-
-    const ground = Matter.Bodies.rectangle(200, window.innerHeight, 2100, 60, {
+    const ground = Matter.Bodies.rectangle(200, window.innerHeight, 3000, 60, {
       isStatic: true,
     });
 
-    Matter.Composite.add(this.engine.world, [boxA, boxB, boxC, boxD, ground]);
+    var size = 50;
+
+    var stack = Matter.Composites.stack(
+      700,
+      600 - 17 - size * 6,
+      12,
+      8,
+      0,
+      0,
+      function (x: any, y: any) {
+        var partA = Matter.Bodies.rectangle(x, y, size, size / 5, {
+            mass: 0,
+            restitution: 0,
+          }),
+          partB = Matter.Bodies.rectangle(x, y, size / 3, size, {
+            mass: 0,
+            restitution: 0,
+            render: partA.render,
+          });
+
+        return Matter.Body.create({
+          parts: [partA, partB],
+        });
+      },
+    );
+
+    Matter.Composite.add(this.engine.world, [
+      ground,
+      stack,
+      boxA,
+      boxB,
+      boxC,
+      boxD,
+    ]);
 
     const mouse = Matter.Mouse.create(this.render.canvas);
     const mouseConstraint = Matter.MouseConstraint.create(this.engine, {
