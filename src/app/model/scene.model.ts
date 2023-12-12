69fac9ab89d3ddde87ea38491eacc37905fb5aa3
Umar Turdiev <ukcturdiev@gmail.com>
2023-12-12 19:29:23 +0000

    Change model from class to interface

--- src/app/model/scene.model.ts
+++ src/app/model/scene.model.ts
@@ -21,64 +21,53 @@ export enum AccelerationUnit {
   MeterPerSecondSquared = 'm/s^2',
 }
 
-export class SceneSettings {
-  gravity: { x: number; y: number; unit: AccelerationUnit } = {
-    x: 0,
-    y: 0,
-    unit: AccelerationUnit.MeterPerSecondSquared,
-  };
-  time_step: number = 0;
+export enum ObjectType {
+  Rectangle = 'rectangle',
+  Circle = 'circle',
+  Polygon = 'polygon',
 }
 
-export class Point {
-  x: number = 0;
-  y: number = 0;
+export interface SceneSettings {
+  gravity: { x: number; y: number; unit: AccelerationUnit };
+  time_step: number;
 }
 
-export abstract class BaseObject {
-  id: number = 0;
-  static: boolean = false;
-  name: string = '';
-  type: string = ''; // Can be 'rectangle', 'circle', or 'polygon'
-  position: Point = { x: 0, y: 0 };
-  rotation: { value: number; unit: AngleUnit } = {
-    value: 0,
-    unit: AngleUnit.Degree,
-  };
-  mass: { value: number; unit: MassUnit } = {
-    value: 0,
-    unit: MassUnit.Kilogram,
-  };
-  friction: { static: number; kinetic: number } = { static: 0, kinetic: 0 };
-  color: string = '';
+export interface Point {
+  x: number;
+  y: number;
 }
 
-export class Rectangle extends BaseObject {
-  dimension: { width: number; height: number; unit: LengthUnit } = {
-    width: 0,
-    height: 0,
-    unit: LengthUnit.Centimeter,
-  };
+export interface BaseObject {
+  id: number;
+  static: boolean;
+  name: string;
+  type: ObjectType;
+  position: Point;
+  rotation: { value: number; unit: AngleUnit };
+  mass: { value: number; unit: MassUnit };
+  friction: { static: number; kinetic: number };
+  color: string;
 }
 
-export class Circle extends BaseObject {
-  radius: { value: number; unit: LengthUnit } = {
-    value: 0,
-    unit: LengthUnit.Centimeter,
-  };
+export interface Rectangle extends BaseObject {
+  dimension: { width: number; height: number; unit: LengthUnit };
 }
 
-export class Polygon extends BaseObject {
-  points: Point[] = [];
+export interface Circle extends BaseObject {
+  radius: { value: number; unit: LengthUnit };
+}
+
+export interface Polygon extends BaseObject {
+  points: Point[];
 }
 
 export type SceneObject = Rectangle | Circle | Polygon;
 
-export class Scene {
-  scene_name: string = '';
-  author: string = '';
-  created_at: string = '';
-  last_modified_at: string = '';
-  settings: SceneSettings = new SceneSettings();
-  objects: SceneObject[] = [];
+export interface Scene {
+  scene_name: string;
+  author: string;
+  created_at: string;
+  last_modified_at: string;
+  settings: SceneSettings;
+  objects: SceneObject[];
 }
