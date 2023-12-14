fbdb8bd988931d3c9ea5923f01937d87a331189f
Umar Turdiev <ukcturdiev@gmail.com>
2023-12-14 16:30:25 +0000

    Implement mode switcher

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
 
+import { Mode } from 'src/app/model/modes.model';
+import { ViewportModesSharedService } from 'src/app/services/viewport-modes-shared.service';
+
 @Component({
   selector: 'menu-bar',
   templateUrl: './menu-bar.component.html',
   styleUrls: ['./menu-bar.component.css', '../../../styles.css'],
 })
 export class MenuBarComponent {
+  constructor(private viewportModesSharedService: ViewportModesSharedService) {}
+
   showDropdown: { [key: string]: boolean } = {};
+  selectedMode: Mode | undefined;
 
   toggleDropdown(menu: string): void {
     this.showDropdown[menu] = !this.showDropdown[menu];
+  }
 
-    console.log(this.showDropdown[menu]);
+  onModeChange() {
+    if (this.selectedMode) {
+      this.viewportModesSharedService.setCurrentMode(this.selectedMode);
+    }
   }
 }
