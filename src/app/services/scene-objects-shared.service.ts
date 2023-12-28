37d55f2a949833e7d794c8cb465646a9d21e47e1
Umar Turdiev <ukcturdiev@gmail.com>
2023-12-28 05:02:27 +0000

    Add function to get object by id

--- src/app/services/scene-objects-shared.service.ts
+++ src/app/services/scene-objects-shared.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ReplaySubject, Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 import { SceneObject } from '../model/scene.model';
 
@@ -16,5 +17,15 @@ export class SceneObjectsSharedService {
 
   public setSceneObjects(data: Map<number, SceneObject>): void {
     this.sceneObjects.next(data);
+  }
+
+  public getSceneObject(id: number): SceneObject | undefined {
+    let sceneObject: SceneObject | undefined;
+
+    this.sceneObjects.pipe(take(1)).subscribe((sceneObjectMap) => {
+      sceneObject = sceneObjectMap.get(id);
+    });
+
+    return sceneObject;
   }
 }
